e8ec65d12d91c3df2fbcda4b7096dd4200549aed
EC2 Default User <ec2-user@ip-172-31-46-112.ec2.internal>
2020-09-07 02:06:16 +0000

    [FIXED] users controller books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def index
+    @user = current_user
+    @books = Book.all
+  end
+
+  def show
+    @user = current_user
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
+    @book = Book.new
   end
 
   def show
     @user = current_user
+    @book = Book.new
+    @books = Book.where "id == ?", current_user.id
   end
 
   def edit
